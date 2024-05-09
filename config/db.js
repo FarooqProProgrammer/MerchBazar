a82d6c194b0fb83780a7e0d2be837f49332915ed
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2024-05-09 21:41:55 +0500

    Code Updated

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 require('dotenv').config();
 
-module.exports = mongoose.connect(process.env.LOCAL_DB).then((res)=>{
-    console.log('Database is Successfully Connect')
-}).catch((error)=>{
-    console.log(error)
-})
-
-// module.exports = mongoose.connect('mongodb+srv://farooq123:farooq123@cluster0.ijdh8yd.mongodb.net/dashboard?retryWrites=true&w=majority&appName=Cluster0').then((res)=>{
+// module.exports = mongoose.connect(process.env.LOCAL_DB).then((res)=>{
 //     console.log('Database is Successfully Connect')
 // }).catch((error)=>{
 //     console.log(error)
 // })
+
+module.exports = mongoose.connect('mongodb+srv://farooq123:farooq123@cluster0.ijdh8yd.mongodb.net/dashboard?retryWrites=true&w=majority&appName=Cluster0').then((res)=>{
+    console.log('Database is Successfully Connect')
+}).catch((error)=>{
+    console.log(error)
+})
