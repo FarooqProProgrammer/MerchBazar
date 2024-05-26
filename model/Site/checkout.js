608963c52c4d8d7b4d046b6d566f578df92f864d
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2024-05-26 11:16:10 +0500

    working updated

---
+++ model/Site/checkout.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const checkoutSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    lastName: String,
+    companyName: String,
+    address: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    regionState: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zipCode: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    orderNotes: String,
+    // cartProductIDs: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Cart' // Assuming you have a Cart model
+    // }]
+});
+
+const Checkout = mongoose.model('Checkout', checkoutSchema);
+
+module.exports = Checkout;
